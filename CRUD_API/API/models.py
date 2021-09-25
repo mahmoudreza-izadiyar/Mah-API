87bb1abc4e48d69fbc1cc2cfc3e4b040afa2fce3
Mahmoudreza-Izadiyar <m.izadiyar@gmail.com>
2021-09-25 19:42:46 +1000

    note api

--- CRUD_API/API/models.py
+++ CRUD_API/API/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=60)
+    context = models.CharField(max_length=500)
+    def __str__(self):
+        return self.title
